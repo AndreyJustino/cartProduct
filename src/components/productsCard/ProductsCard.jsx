cef6d808c93ca41a50b6a839da189ba1ae5e315e
AndreyJustino <andreyjustino2020@gmail.com>
2024-09-05 17:15:59 -0300

    sdasds

--- src/components/productsCard/ProductsCard.jsx
+++ src/components/productsCard/ProductsCard.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import "./styleProductsCard.css";
 
 function ProductsCard({urlImg, price, title}) {
-  
+  function evento(img, titulo, preco){
+    
+  }
   return (
     <>
-      <li>
+      <li onClick={() => {
+        evento(urlImg, title, price);
+      }}>
         <div class="card">
           <div class="image">
             <img src={urlImg} />
           </div>
           <span class="title">{title}</span>
           <span class="price">R${price}</span>
+          <button className="button-add-to-cart">ADD</button>
         </div>
       </li>
     </>
